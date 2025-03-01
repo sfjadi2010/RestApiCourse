20242ae1c4c3c8d44367b164e8156491fbe23b9d
sfjadi2010 <sfjadi2010@live.com>
2025-03-01 17:05:17 -0500

    Added CURD operations.

---
+++ Movies.Application/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Movies.Application.Models;
+
+public class Movie
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+    public required int YearOfRelease { get; init; }
+    public required List<string> Genres { get; init; } = new();
+}
